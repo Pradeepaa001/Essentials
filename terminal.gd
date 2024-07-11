e56e376f9d59e4305054d56a54252a0aed8846eb
Yaswitha <323103210032.yaswitha@gvpcew.ac.in>
2024-07-11 20:25:11 +0530

    Added LineEdit node to take input, and fixed the code for output. things need to be fixed: Only one line of input can be given and interfaces for input and output are different. Execution of commands

--- terminal.gd
+++ terminal.gd
@@ -1,25 +1,22 @@
 extends Node2D
 
-
 var input_enabled : bool = true
+var entered: bool = false
 
-func _on_text_changed(new_text: String):
-
-	
-
-	var cmd = get_node("Input")
-
-	var response = processCommand(cmd)
-	
-	var output = $Output
-	output.text += response + "\n"
-	#text_edit2.cursor_set_line(text_edit2.get_line_count())
+#func _on_text_changed(new_text: String):
+	#var cmd = get_node("Input")
+	#print("new text is", new_text)
+	#var response = processCommand(cmd)
+	#var output = $Output
+	#output.text += response + "\n"
+	#entered = false
+	##text_edit2.cursor_set_line(text_edit2.get_line_count())
 
 func processCommand(cmd):
-	var response = "\n"
-	match cmd:
+	var response: String = "\n"
+	match str(cmd):
 		"help":
-			response += "Available commands:\n  help - Display available commands\n  ls - List files\n"
+			response += "Available commands: help - Display available commands ls - List files"
 		"ls":
 			response += "File1.txt\nFile2.txt"
 		"":
@@ -28,4 +25,11 @@ func processCommand(cmd):
 			response += "Command not recognized: " + cmd
 	return response
 
+func _on_line_edit_text_submitted(cmd):
+	
+	get_node("Input")
+	var response = processCommand(cmd) + "\n"
+	var output = $output
+	output.text += response
+	entered = false
 
