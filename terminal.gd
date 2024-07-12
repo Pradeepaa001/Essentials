ad61f6d2f0ca7cb357f0c8ba076da33a7fe9457c
Hitha Ankam <23wh1a1289@bvrithyderabad.edu.in>
2024-07-12 17:08:52 +0530

    Figured OS.execute() to implement a terminal for checking the shell script commands few commands work

--- terminal.gd
+++ terminal.gd
@@ -1,35 +1,39 @@
 extends Node2D
 
-var input_enabled : bool = true
 var entered: bool = false
 
-#func _on_text_changed(new_text: String):
-	#var cmd = get_node("Input")
-	#print("new text is", new_text)
-	#var response = processCommand(cmd)
-	#var output = $Output
-	#output.text += response + "\n"
-	#entered = false
-	##text_edit2.cursor_set_line(text_edit2.get_line_count())
 
-func processCommand(cmd):
+func processCommand(cmd: String) -> String:
 	var response: String = "\n"
-	match str(cmd):
-		"help":
-			response += "Available commands: help - Display available commands ls - List files"
-		"ls":
-			response += "File1.txt\nFile2.txt"
-		"":
-			response = ""
-		_:
-			response += "Command not recognized: " + cmd
+	
+	if cmd.strip_edges() in ["help", "ls"]:
+		match cmd.strip_edges():
+			"help":
+				response += "Available commands: help - Display available commands, ls - List files"
+			"ls":
+				response += "File1.txt\nFile2.txt"
+	else:
+		var output = []
+		var output_buffer = PackedStringArray()
+		var error_buffer = PackedStringArray()
+		#var error_code = helpers.exec(cmd)
+		var error_code = OS.execute("wsl.exe", [cmd, "cd %TEMP% && dir"], output, true)
+		#var error_code = helpers.exec()
+		print(output)
+		if error_code == 0:
+			response += String(output[-1])
+		else:
+			response += "Command failed with error code: " + str(error_code) + "\n" + String(output[-1])
+
+	
 	return response
 
-func _on_line_edit_text_submitted(cmd):
-	
+func _on_line_edit_text_submitted(cmd: String):
 	get_node("Input")
 	var response = processCommand(cmd) + "\n"
 	var output = $output
 	output.text += response
 	entered = false
+
+
 
