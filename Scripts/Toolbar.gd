d93e8b6e88e4ce1a98735eeb2fede68af9bd4ed6
Pradeepaa M <mpradeepaa2020@gmail.com>
2024-07-20 18:52:13 +0530

    Added quit in toolbar with confirmation dialogue box

--- Scripts/Toolbar.gd
+++ Scripts/Toolbar.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+#@onready var quit_button = $Button2
 
+
+@onready var confirm_dialog = $ConfirmationDialog
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,3 +16,13 @@ func _process(delta):
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/level_selector.tscn")
+
+
+func _on_button_2_pressed():
+	confirm_dialog.popup_centered()
+
+func _on_confirmation_dialog_canceled():
+	pass
+	
+func _on_confirmation_dialog_confirmed():
+	get_tree().quit()
