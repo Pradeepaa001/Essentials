2b6a143d15b70aa3725b261a0f31cd49013b6cc4
Pradeepaa M <mpradeepaa2020@gmail.com>
2024-07-20 11:31:38 +0530

    Added ls c* and pwd to level2

--- Scripts/LEVEL2.gd
+++ Scripts/LEVEL2.gd
@@ -1,9 +1,19 @@
 extends Node2D
 
 var level_title = "The Master of files"
-var level_description = "Welcome to the new planet captain! Now you are on a mission to fix the things here in order to help the aliens live a peaceful life.See around (ls), ask for help(ls --help) and find the hidden problems(ls -a). Dive in!"
-var level_setup_commands = ["ls", "ls --help", "ls -a"]
-var level_congrats_message = "Well done, Explorer! You've gathered your first resources."
+var level_description = """
+Learn to list the contents of a directory with ls commands.
+
+Instructions:
+1. Use `ls` to view the contents of a directory. Example: `ls`
+2. Use `ls --help` to know how to use ls. Example: `ls --help`
+3. Use `ls -a` to view hidden files. Example: `ls -a`
+4. Use `ls *<char>` to view files and sub-directories that start with specified character. Example: `ls r*`
+	note: This lists nothings if there are no contents starting with the character 
+5. Use `pwd` to see the path of current working directory. Example: `rmdir data`
+"""
+var level_setup_commands = ["ls", "ls --help", "ls -a", "ls r*", "pwd"]
+var level_congrats_message = "Well done, Explorer! You've completed the first level"
 @onready var termi = $Terminal
 var all_inputs = []
 
@@ -30,6 +40,17 @@ func task2_status():
 func task3_status() -> bool:
 	all_inputs = termi.get_input_list()
 	return "ls -a" in all_inputs
+	
+func task4_status() -> bool:
+	all_inputs = termi.get_input_list()
+	for commands in all_inputs:
+		if "ls " and "*" in commands:
+			return true
+	return false
+	
+func task5_status() -> bool:
+	all_inputs = termi.get_input_list()
+	return "pwd" in all_inputs
 	
 func update_icons():
 	if task1_status():
@@ -60,6 +81,22 @@ func update_icons():
 	else:
 		print("task3:", task3_status())
 		$ColorRect3.color  = Color(1,0,0)
+	
+	if task4_status():
+		print("task3:", task4_status())
+		$ColorRect4.color  = Color(0,1,0)
+
+	else:
+		print("task3:", task4_status())
+		$ColorRect4.color  = Color(1,0,0)
+	
+	if task5_status():
+		print("task3:", task5_status())
+		$ColorRect.color  = Color(0,1,0)
+
+	else:
+		print("task3:", task5_status())
+		$ColorRect.color  = Color(1,0,0)
 
 func _on_check_pressed():
 	var output = $RichTextLabel
